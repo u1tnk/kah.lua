70b8d0ed7243edbeb3087e09218a5f5795806083
yuichi tanaka <u1tnk1123@gmail.com>
2012-06-08 17:18:08 +0900

    モジュール排除、デバグコード削除

--- corona_http_client.lua
+++ corona_http_client.lua
@@ -1,7 +1,6 @@
-module(..., package.seeall) 
 local _u = require 'utils'
 
-Client = {
+local M = {
   headers = {}
   , protocol = "http"
   , host = "localhost"
@@ -9,18 +8,18 @@ Client = {
   , port = 80
 }
 
-function Client:new(o)
+function M:new(o)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
   return o
 end
 
-function Client:addHeader(name, value)
+function M:addHeader(name, value)
   self.headers[name] = value
 end
 
-function Client:request(options)
+function M:request(options)
     local defaults = {
       headers = self.headers
       , method = self.method
@@ -38,7 +37,6 @@ function Client:request(options)
     o.headers = headers
 
     local function networkListener(e)
-      p(e.response, "生レス")
       if e.isError then
         o.onError()
       else
@@ -52,6 +50,7 @@ function Client:request(options)
     if queryParams then
       url = url .. "?"  .. _u.makeQuery(queryParams)
     end
-    p(url, "url")
     network.request(url, options.method, networkListener, params)
 end
+
+return M
