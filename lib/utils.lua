3983d1f08bf20045e47f36d66e58ca4de00d35af
arumons <takohati1@gmail.com>
2012-08-13 10:43:02 +0900

    ファイルの存在有無をチェックする関数追加

--- lib/utils.lua
+++ lib/utils.lua
@@ -83,6 +83,25 @@ function M.dump(data, name)
 end
 M.p = M.dump
 
+
+-- import form http://bsharpe.com/code/coronasdk-how-to-know-if-a-file-exists
+function M.fileExists(fileName, base)
+  assert(fileName, "fileName is missing")
+  local base = base or system.ResourceDirectory
+  local filePath = system.pathForFile( fileName, base )
+  local exists = false
+ 
+  if (filePath) then -- file may exist. won't know until you open it
+    local fileHandle = io.open( filePath, "r" )
+    if (fileHandle) then -- nil if no file found
+      exists = true
+      io.close(fileHandle)
+    end
+  end
+ 
+  return(exists)
+end
+
 -- Helper function that loads a file into ram.
 function M.loadFile(path)
   local intmp = io.open(path, 'r')
